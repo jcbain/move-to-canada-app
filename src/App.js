9507ea7c8e0160996cefa68740897115adf9b215
jcbain <jamescbain@gmail.com>
2020-06-16 23:02:55 -0600

    compartmentalize AnimatedRoute into its own component

--- src/App.js
+++ src/App.js
@@ -8,29 +8,28 @@ import gsap from 'gsap';
 // import Map from './components/Map';
 import Map from './components/Map2'
 import Button from '@material-ui/core/Button';
-gsap.registerPlugin(ScrollTrigger);
 
 
 function App(props) {
-  const [scale, setScale] = useState(3);
+  const [scale, setScale] = useState(4);
 
-  const zoomInScale = () => {
-    setScale(scale + 1);
-  }
-  const zoomOutScale = () => {
-    if(scale > 1){
-      setScale(scale - 1)
-    }
-  }
+  // const zoomInScale = () => {
+  //   setScale(scale + 1);
+  // }
+  // const zoomOutScale = () => {
+  //   if(scale > 1){
+  //     setScale(scale - 1)
+  //   }
+  // }
 
 
   return(
     <div>
-      <div className="update-buttons">
+      {/* <div className="update-buttons">
         <Button variant="outlined" onClick={zoomInScale}>Zoom In</Button>
         <Button variant="outlined" onClick={zoomOutScale}>Zoom Out</Button>
            <p>Scale {scale}</p>
-      </div>
+      </div> */}
       <Map className="map"
           width={500}
           height={500}
--- src/components/Map2.js
+++ src/components/Map2.js
@@ -13,6 +13,7 @@ import route from '../data/to_calgary';
 
 gsap.registerPlugin(ScrollTrigger);
 
+
 const MapDiv = styled.div`
     top: 0;
     position: fixed;
@@ -29,58 +30,110 @@ const TmpDiv = styled.div`
     width: 20vw;
 `
 
+const AnotherTmpDiv = styled.div`
+    position: absolute;
+    left: 90vw;
+`
+
+const AnimatedRoute = forwardRef((props, ref) => {
+    useEffect(() => {
+        gsap.to(ref.current, {
+          scrollTrigger: {
+              trigger: props.triggerRef.current,
+              markers: true,
+              start: "top 90%",
+              end: "top 15%",
+              scrub: true,
+              toggleActions: "restart pause reverse pause"     
+            },
+            strokeDashoffset: `${0}`,
+        });
+      }, [props.triggerRef, ref]);
+
+      return (
+        <MapShape
+            ref={ref}
+            useFeatures={props.useFeatures}
+            className={props.className}
+            data={props.data}
+            path={props.path}
+            stroke={props.stroke}
+            strokeWidth={props.strokeWidth}
+            fill={props.fill}
+            usePathMeasure={props.usePathMeasure}
+        ></MapShape> 
+      )
+
+})
 
 export default function Map(props){
-    const [animation, setAnimation] = useState();
+    const [distanceAnimation, setDistanceAnimation] = useState();
+
+    const [distance, setDistance] = useState(0);
+    const [prevDistance, setPrevDistance] = useState(distance);
+    let tmpRef = useRef(null)
     let firstLegDivRef = useRef(null);
     let firstLegRef = useRef(null);
     let secondLegDivRef = useRef(null);
     let secondLegRef = useRef(null);
 
-
     const projection = createProjection(props.width, props.height, props.scale, props.centerLong, props.centerLat);
     const path = geoPath().projection(projection);
 
+    const movePath = (targetNode, triggerNode, markers) => {
+        return gsap.to(targetNode, {
+            scrollTrigger : {
+                trigger: triggerNode,
+                markers: markers,
+                start: "top 90%",
+                end: "top 15%",
+                scrub: true,
+                toggleActions: "restart pause reverse pause"     
+            },
+            strokeDashoffset: `${0}`,
+        })
+    }
 
-    useEffect(() => {
+    // const moveDistance = (targetNode, triggerNode, startDistance, endDistance) => {
+    //     return gsap.to(targetNode, {
+    //         scrollTrigger: {
+    //             trigger: triggerNode,
+    //             start: "top 90%",
+    //             end: "top 15%",
+    //             scrub: true,
+    //             onEnter: () => setDistance(startDistance),
+    //             onLeaveBack: () => setDistance(startDistance),
+    //             // onLeave: () => setDistance(endDistance),
+    //             // onLeaveBack: () => setDistance(startDistance),
+    //             // onEnterBack: () => setDistance(startDistance),
+    //             // onLeaveBack: () => setDistance(endDistance),
+    //             toggleActions: "restart pause reverse pause"
+    //         },
+    //         innerHTML: endDistance
+    //     })
+    // }
 
-        setAnimation(
-            gsap.to(firstLegRef.current, {
-                scrollTrigger : {
-                    trigger: firstLegDivRef,
-                    markers: true,
-                    start: "top 90%",
-                    end: "top 15%",
-                    scrub: 0,
-                    toggleActions: "restart pause reverse pause"
-                },
-                strokeDashoffset: `${0}`,
-                rotation: 0,
-                duration: 20
-            }
-            ),
-            gsap.to(secondLegRef.current, {
-                scrollTrigger : {
-                    trigger: secondLegDivRef,
-                    markers: true,
-                    start: "top 90%",
-                    end: "top 15%",
-                    scrub: 0,
-                    toggleActions: "restart pause reverse pause"
-                },
-                strokeDashoffset: `${0}`,
-                rotation: 0,
-                duration: 20
-            }
-            )
-        );
-    }, []);
+
+    // useEffect(() => {
+    //     setDistanceAnimation(
+    //         // moveDistance(tmpRef, firstLegDivRef, 0,  122),
+    //         // moveDistance(tmpRef, secondLegDivRef, 122, 143)
+
+
+    //     );
+    //     // setDistanceAnimation(
+    //     //     moveDistance2(tmpRef.current, secondLegDivRef, 122, 143)
+
+    //     // );
+    // }, [distance])
+
 
 
     return (
         <div>
         
             <MapDiv>
+                <AnotherTmpDiv className='.tmp' ref={element => {tmpRef = element;}}>{distance}</AnotherTmpDiv>
 
                 <MapSvg
                     className={props.className}
@@ -106,7 +159,33 @@ export default function Map(props){
                             strokeWidth={'0.25px'}
                             fill={'#fff'}>
                         </MapShape>
-                        <MapShape key='route'
+                        <AnimatedRoute 
+                            key={'route-1'}
+                            ref={firstLegRef}
+                            triggerRef={firstLegDivRef}
+                            useFeatures={false}
+                            className={'route'}
+                            data={[route.features[0]]}
+                            path={path}
+                            stroke={'#000'}
+                            strokeWidth={'2px'}
+                            fill={'none'}
+                            usePathMeasure={true}>
+                        </AnimatedRoute>
+                        <AnimatedRoute 
+                            key={'route-2'}
+                            ref={secondLegRef}
+                            triggerRef={secondLegDivRef}
+                            useFeatures={false}
+                            className={'route'}
+                            data={[route.features[1]]}
+                            path={path}
+                            stroke={'#000'}
+                            strokeWidth={'2px'}
+                            fill={'none'}
+                            usePathMeasure={true}>
+                        </AnimatedRoute>
+                        {/* <MapShape key='route'
                             ref={firstLegRef}
                             useFeatures={false}
                             className={'route'}
@@ -116,8 +195,8 @@ export default function Map(props){
                             strokeWidth={'2px'}
                             fill={'none'}
                             usePathMeasure={true}
-                        ></MapShape> 
-                        <MapShape key='route2'
+                        ></MapShape>  */}
+                        {/* <MapShape key='route2'
                             ref={secondLegRef}
                             useFeatures={false}
                             className={'route'}
@@ -127,7 +206,7 @@ export default function Map(props){
                             strokeWidth={'2px'}
                             fill={'none'}
                             usePathMeasure={true}
-                        ></MapShape> 
+                        ></MapShape>  */}
                 </MapSvg>
 
             </MapDiv>
@@ -136,19 +215,18 @@ export default function Map(props){
                 Ut eu sem integer vitae justo eget. Natoque penatibus et magnis dis. Felis bibendum ut tristique et. Gravida neque convallis a cras semper. Porttitor massa id neque aliquam. Amet mattis vulputate enim nulla aliquet porttitor lacus luctus accumsan. Nulla aliquet porttitor lacus luctus accumsan tortor posuere. Orci porta non pulvinar neque. Est ullamcorper eget nulla facilisi etiam dignissim diam quis enim. Est placerat in egestas erat. Iaculis nunc sed augue lacus viverra vitae. Pellentesque elit ullamcorper dignissim cras tincidunt lobortis. Quam adipiscing vitae proin sagittis nisl rhoncus mattis. Ornare arcu dui vivamus arcu felis bibendum ut. Vulputate ut pharetra sit amet. Diam maecenas sed enim ut. A diam sollicitudin tempor id eu. Malesuada nunc vel risus commodo viverra maecenas accumsan.
                 Varius quam quisque id diam vel. Quisque egestas diam in arcu cursus euismod. Cursus risus at ultrices mi. Eleifend donec pretium vulputate sapien nec sagittis. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Sit amet cursus sit amet dictum sit amet justo donec. Ante metus dictum at tempor. Donec ac odio tempor orci. Pulvinar mattis nunc sed blandit. Amet nisl suscipit adipiscing bibendum est. Nulla aliquet enim tortor at auctor urna nunc id cursus.
             </TmpDiv>
-            <TmpDiv ref={element => {firstLegDivRef = element;}}>
+            <TmpDiv ref={firstLegDivRef}>
                 ...and so Jennifer set off from Columbia, MO, our home of 10ish years to where we grew up in Kansas City, MO. This was the end of a chapter, perhaps even a book and a new one was about to begin. We had thousands of miles ahead of us, three border crossings, 
                 nearly half of the states in the US and a handful of provinces yet ahead.
                 Varius quam quisque id diam vel. Quisque egestas diam in arcu cursus euismod. Cursus risus at ultrices mi. Eleifend donec pretium vulputate sapien nec sagittis. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Sit amet cursus sit amet dictum sit amet justo donec. Ante metus dictum at tempor. Donec ac odio tempor orci. Pulvinar mattis nunc sed blandit. Amet nisl suscipit adipiscing bibendum est. Nulla aliquet enim tortor at auctor urna nunc id cursus.
                 Varius quam quisque id diam vel. Quisque egestas diam in arcu cursus euismod. Cursus risus at ultrices mi. Eleifend donec pretium vulputate sapien nec sagittis. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Sit amet cursus sit amet dictum sit amet justo donec. Ante metus dictum at tempor. Donec ac odio tempor orci. Pulvinar mattis nunc sed blandit. Amet nisl suscipit adipiscing bibendum est. Nulla aliquet enim tortor at auctor urna nunc id cursus.
 
             </TmpDiv>
-            <TmpDiv ref={element => {secondLegDivRef = element;}}>
+            <TmpDiv ref={secondLegDivRef}>
                 ...and so Jennifer set off from Columbia, MO, our home of 10ish years to where we grew up in Kansas City, MO. This was the end of a chapter, perhaps even a book and a new one was about to begin. We had thousands of miles ahead of us, three border crossings, 
                 nearly half of the states in the US and a handful of provinces yet ahead.
                 Varius quam quisque id diam vel. Quisque egestas diam in arcu cursus euismod. Cursus risus at ultrices mi. Eleifend donec pretium vulputate sapien nec sagittis. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Sit amet cursus sit amet dictum sit amet justo donec. Ante metus dictum at tempor. Donec ac odio tempor orci. Pulvinar mattis nunc sed blandit. Amet nisl suscipit adipiscing bibendum est. Nulla aliquet enim tortor at auctor urna nunc id cursus.
                 Varius quam quisque id diam vel. Quisque egestas diam in arcu cursus euismod. Cursus risus at ultrices mi. Eleifend donec pretium vulputate sapien nec sagittis. Pharetra diam sit amet nisl suscipit adipiscing bibendum. Nullam eget felis eget nunc lobortis mattis aliquam faucibus. Sit amet cursus sit amet dictum sit amet justo donec. Ante metus dictum at tempor. Donec ac odio tempor orci. Pulvinar mattis nunc sed blandit. Amet nisl suscipit adipiscing bibendum est. Nulla aliquet enim tortor at auctor urna nunc id cursus.
-
             </TmpDiv>
 
         </div>
