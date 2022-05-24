65574004030700d3604ed61a461dbc4809cbb53f
jcbain <jamescbain@gmail.com>
2022-05-23 20:38:14 -0600

    refactor: start fresh

--- src/App.js
+++ src/App.js
@@ -1,108 +1,25 @@
-import React, { useState } from "react";
-import styled, { ThemeProvider } from "styled-components";
-import "./App.css";
-import { Helmet } from "react-helmet";
-import { flattenDepth } from "lodash";
-
-import { GlobalStyle, lightTheme } from "./theme/theme";
-import Story from "./components/Story";
-
-// import Map from './components/Map';
-// import Map from "./components/Map";
-// import BottomMap from "./components/BottomMap";
-
-// import canadianProvinces from "./data/canada";
-// import usStates from "./data/us-states";
-// import route from "./data/to_calgary";
-// import route2 from "./data/calgary_to_kc";
-// import route3 from "./data/kc_to_kc";
-// import route4 from "./data/kc_back_calgary";
-
-// const legCoordsFlat = [route, route2, route3, route4].map((d) => {
-//   return d.features.map((r) => {
-//     return r.geometry.coordinates[0].pop();
-//   });
-// });
-// const legCoords = flattenDepth(legCoordsFlat, 1);
+import { useState } from "react";
+import RoutePath from "./components/RoutePath";
+import { geoEquirectangular, geoPath } from "d3-geo";
+import routes from "./data/calgary_to_kc.json";
 
 function App() {
-  // const [scale, setScale] = useState(2);
+  const [offset, setOffset] = useState(0);
 
-  // const zoomInScale = () => {
-  //   setScale(scale + 1);
-  // }
-  // const zoomOutScale = () => {
-  //   if(scale > 1){
-  //     setScale(scale - 1)
-  //   }
-  // }
-
+  const projection = geoEquirectangular().fitSize([800, 500], routes);
+  const pathGenerator = geoPath().projection(projection);
+  const pathData = pathGenerator(routes.features[12]);
   return (
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
-      <div className="application">
-        <Helmet>
-          <meta charSet="utf-8" />
-          <title>Journey to Canada</title>
-          <link
-            href="https://fonts.googleapis.com/css2?family=Itim&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Lobster&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap"
-            rel="stylesheet"
-          />
-        </Helmet>
-        <Story />
-        {/* <Map
-          className="map"
-          width={500}
-          height={500}
-          scale={scale}
-          centerLong={92.35361}
-          centerLat={38.95133}
-          canadianProvinces={canadianProvinces}
-          usStates={usStates}
-          routes={[route, route2, route3, route4]}
-          legCoords={legCoords}
-        ></Map>
-        <BottomMap
-          className="bottomMap"
-          width={500}
-          height={500}
-          scale={scale - 0.5}
-          centerLong={92.35361}
-          centerLat={48.95133}
-          canadianProvinces={canadianProvinces}
-          usStates={usStates}
-          routes={[route, route2, route3, route4]}
-        ></BottomMap> */}
-      </div>
-    </ThemeProvider>
+    <div className="App">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 841.9 595.3">
+        <g transform="translate(0,0) scale(1)">
+          <RoutePath pathData={pathData} percentShown={50.5} />
+        </g>
+      </svg>
+      <button onClick={() => setOffset((prev) => prev + 1)}>
+        Update {offset}
+      </button>
+    </div>
   );
 }
 
---
+++ src/components/RoutePath/index.js
@@ -0,0 +1,42 @@
+import { useState, useEffect, useRef } from "react";
+
+const RoutePath = ({ percentShown, pathData }) => {
+  const ref = useRef();
+  const [pathLength, setLength] = useState(null);
+
+  const distance = pathLength - (percentShown / 100) * pathLength;
+  console.log("distance", distance);
+
+  useEffect(() => {
+    if (ref.current) {
+      console.log(ref.current.getTotalLength());
+      setLength(ref.current.getTotalLength());
+    }
+  }, [setLength]);
+
+  return (
+    <>
+      <path
+        ref={ref}
+        d={pathData}
+        fill={"none"}
+        stroke="lightgray"
+        strokeWidth={5}
+      />
+      ;
+      {pathLength !== null && (
+        <path
+          d={pathData}
+          fill={"none"}
+          stroke="blue"
+          strokeWidth={5}
+          strokeDasharray={`${pathLength} ${pathLength}`}
+          strokeDashoffset={`-${distance}`}
+        />
+      )}
+      ;
+    </>
+  );
+};
+
+export default RoutePath;
